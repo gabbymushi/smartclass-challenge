0d0354a6c51209693999b0a4b8bae151c3f70add
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2022-01-29 10:53:26 +0300

    add new routes

--- dist/modules/index.js
+++ dist/modules/index.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.initializeRoutes = void 0;
 const user_routes_1 = require("./users/user.routes");
 const auth_routes_1 = require("./auth/auth.routes");
-const config_routes_1 = require("./configurations/config.routes");
 const transaction_routes_1 = require("./transactions/transaction.routes");
+const order_routes_1 = require("./orders/order.routes");
+const item_routes_1 = require("./items/item.routes");
 const initializeRoutes = (app) => {
     app.use('/api/v1/users/', user_routes_1.UserRoutes);
     app.use('/api/v1/auth/', auth_routes_1.AuthRoutes);
-    app.use('/api/v1/configs/', config_routes_1.ConfigRoutes);
     app.use('/api/v1/deals/', transaction_routes_1.TransactionRoutes);
+    app.use('/api/v1/items/', item_routes_1.ItemRoutes);
+    app.use('/api/v1/orders/', order_routes_1.OrderRoutes);
 };
 exports.initializeRoutes = initializeRoutes;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvbW9kdWxlcy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFDQSxxREFBaUQ7QUFDakQsb0RBQWdEO0FBQ2hELGtFQUE4RDtBQUU5RCwwRUFBc0U7QUFFL0QsTUFBTSxnQkFBZ0IsR0FBRyxDQUFDLEdBQXdCLEVBQUUsRUFBRTtJQUN6RCxHQUFHLENBQUMsR0FBRyxDQUFDLGdCQUFnQixFQUFFLHdCQUFVLENBQUMsQ0FBQztJQUN0QyxHQUFHLENBQUMsR0FBRyxDQUFDLGVBQWUsRUFBRSx3QkFBVSxDQUFDLENBQUM7SUFDckMsR0FBRyxDQUFDLEdBQUcsQ0FBQyxrQkFBa0IsRUFBRSw0QkFBWSxDQUFDLENBQUM7SUFDMUMsR0FBRyxDQUFDLEdBQUcsQ0FBQyxnQkFBZ0IsRUFBRSxzQ0FBaUIsQ0FBQyxDQUFDO0FBQ2pELENBQUMsQ0FBQTtBQUxZLFFBQUEsZ0JBQWdCLG9CQUs1QiJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvbW9kdWxlcy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFDQSxxREFBaUQ7QUFDakQsb0RBQWdEO0FBRWhELDBFQUFzRTtBQUN0RSx3REFBb0Q7QUFDcEQscURBQWlEO0FBRTFDLE1BQU0sZ0JBQWdCLEdBQUcsQ0FBQyxHQUF3QixFQUFFLEVBQUU7SUFDekQsR0FBRyxDQUFDLEdBQUcsQ0FBQyxnQkFBZ0IsRUFBRSx3QkFBVSxDQUFDLENBQUM7SUFDdEMsR0FBRyxDQUFDLEdBQUcsQ0FBQyxlQUFlLEVBQUUsd0JBQVUsQ0FBQyxDQUFDO0lBQ3JDLEdBQUcsQ0FBQyxHQUFHLENBQUMsZ0JBQWdCLEVBQUUsc0NBQWlCLENBQUMsQ0FBQztJQUM3QyxHQUFHLENBQUMsR0FBRyxDQUFDLGdCQUFnQixFQUFFLHdCQUFVLENBQUMsQ0FBQztJQUN0QyxHQUFHLENBQUMsR0FBRyxDQUFDLGlCQUFpQixFQUFFLDBCQUFXLENBQUMsQ0FBQztBQUM1QyxDQUFDLENBQUE7QUFOWSxRQUFBLGdCQUFnQixvQkFNNUIifQ==
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { UserRoutes } from './users/user.routes';
 import { AuthRoutes } from './auth/auth.routes';
-import { ConfigRoutes } from './configurations/config.routes';
 import {isAuthenticated} from './auth/auth.controller'
 import { TransactionRoutes } from './transactions/transaction.routes';
+import { OrderRoutes } from './orders/order.routes';
+import { ItemRoutes } from './items/item.routes';
 
 export const initializeRoutes = (app: express.Application) => {
     app.use('/api/v1/users/', UserRoutes);
     app.use('/api/v1/auth/', AuthRoutes);
-    app.use('/api/v1/configs/', ConfigRoutes);
     app.use('/api/v1/deals/', TransactionRoutes);
+    app.use('/api/v1/items/', ItemRoutes);
+    app.use('/api/v1/orders/', OrderRoutes);
 }
